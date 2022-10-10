4a5bcdfb7b9b8012e3337a2d1d7c915b5b41bfc5
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2022-10-10 09:35:54 -0400

    cleanup code and add custom alias and ttl features

--- url_shortening/mytinyurl/__init__.py
+++ url_shortening/mytinyurl/__init__.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from urllib.parse import quote_plus
 
 import requests
 from flask import (
@@ -16,6 +17,13 @@ from . import mongo
 
 APP_URL = "127.0.0.1:5000"
 DEFAULT_USER = "anonymous"
+TTL_TO_HOURS = {
+    "1h": 1,
+    "1d": 24,
+    "1w": 24 * 7,
+    "1m": 24 * 30,
+    "1y": 24 * 365,
+}
 
 mongo_client = mongo.Client()
 
@@ -34,22 +42,34 @@ def create_app(test_config=None):
         msg = None
         user_urls = []
 
+        if request.method == "POST":
+            username = session.get("username", DEFAULT_USER)
+            long_url = request.form["longurl"]
+            alias = request.form["custom-alias"]
+            ttl = TTL_TO_HOURS[request.form["ttl"]]
+
+            if alias:
+                alias = quote_plus(alias)
+                if mongo_client.get_url(alias) is not None:
+                    msg = f"Error: alias '{alias}' already exists"
+            else:
+                alias_host = os.getenv("ALIAS_SERVICE_HOST")
+                alias_port = os.getenv("ALIAS_SERVICE_PORT")
+                alias = requests.get(
+                    f"http://{alias_host}:{alias_port}/get-alias"
+                ).json()["alias"]
+
+            if msg is None:
+                mongo_client.create_url(alias, long_url, username, ttl)
+                msg = f"Created short URL: {APP_URL}/{alias}"
+
         if g.user:
             user_urls = mongo_client.get_urls_by_user(g.user["_id"])
             for u in user_urls:
                 u["alias"] = f"{APP_URL}/{u['_id']}"
                 u["created_on"] = u["created_on"].strftime("%m/%d/%Y %H:%M:%S")
+                u["ttl"] = u["ttl"].strftime("%m/%d/%Y %H:%M:%S")
 
-        if request.method == "POST":
-            username = session.get("username", DEFAULT_USER)
-            long_url = request.form["longurl"]
-            alias_host = os.getenv("ALIAS_SERVICE_HOST")
-            alias_port = os.getenv("ALIAS_SERVICE_PORT")
-            alias = requests.get(
-                f"http://{alias_host}:{alias_port}/get-alias"
-            ).json()["alias"]
-            mongo_client.create_url(alias, long_url, username, 24)
-            msg = f"Created short URL: {APP_URL}/{alias}"
 
         return render_template("index.html", message=msg, myurls=user_urls)
 
