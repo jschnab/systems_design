4870927ce954316a9d8eb70ac01abcc9ee2ac28f
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2023-01-12 19:13:30 -0500

    s3 module has function dedicated to call aws

--- pastebin/mygists/s3.py
+++ pastebin/mygists/s3.py
@@ -1,5 +1,3 @@
-import functools
-
 import boto3
 import botocore
 
@@ -9,50 +7,45 @@ from .config import config
 S3_CLIENT = boto3.client("s3")
 
 
-def manage_exceptions(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except botocore.exceptions.ClientError as e:
-            rcode = return_codes.UNKNOWN_ERROR
-            ecode = e.response["Error"]["Code"]
-            if ecode == "AccessDenied":
-                rcode = return_codes.ACCESS_DENIED
-            elif ecode == "NoSuchBucket":
-                rcode = return_codes.S3_BUCKET_NOT_EXISTS
-            elif ecode == "NoSuchKey":
-                rcode = return_codes.S3_KEY_NOT_EXISTS
-            return rcode, None
-
-    return wrapper
-
-
-def check_aws_response(response):
-    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-        return return_codes.UNKNOWN_ERROR
-    return return_codes.OK
-
-
-@manage_exceptions
-def put_text(key, text_body):
-    response = S3_CLIENT.put_object(
-        Body=text_body.encode(config["text_storage"]["encoding"]),
-        Bucket=config["text_storage"]["s3_bucket"],
-        Key=key,
-    )
-    return return_codes.OK, response
-
-
-@manage_exceptions
-def get_text(text_id):
-    response = S3_CLIENT.get_object(
-        Bucket=config["text_storage"]["s3_bucket"], Key=text_id,
-    )
-    rcode = check_aws_response(response)
-    if rcode is return_codes.OK:
-        return (
-            rcode,
-            response["Body"].read().decode(config["text_storage"]["encoding"]),
-        )
+def call_s3_client(method, *args, **kwargs):
+    rcode = return_codes.OK
+    response = None
+    try:
+        response = getattr(S3_CLIENT, method)(*args, **kwargs)
+        if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+            rcode = return_codes.UNKOWN_ERROR
+    except botocore.exceptions.ClientError as e:
+        rcode = return_codes.UNKNOWN_ERROR
+        ecode = e.response["Error"]["Code"]
+        if ecode == "AccessDenied":
+            rcode = return_codes.ACCESS_DENIED
+        elif ecode == "NoSuchBucket":
+            rcode = return_codes.S3_BUCKET_NOT_EXISTS
+        elif ecode == "NoSuchKey":
+            rcode = return_codes.S3_KEY_NOT_EXISTS
     return rcode, response
+
+
+def put_text(key, text_body):
+    rcode, response = call_s3_client(
+        "put_object",
+        **dict(
+            Body=text_body.encode(config["text_storage"]["encoding"]),
+            Bucket=config["text_storage"]["s3_bucket"],
+            Key=key,
+        ),
+    )
+    return rcode, response
+
+
+def get_text(text_id):
+    rcode, response = call_s3_client(
+        "get_object",
+        **dict(Bucket=config["text_storage"]["s3_bucket"], Key=text_id),
+    )
+    if rcode is not return_codes.OK:
+        return rcode, response
+    return (
+        rcode,
+        response["Body"].read().decode(config["text_storage"]["encoding"]),
+    )
