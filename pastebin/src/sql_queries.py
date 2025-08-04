3701f36aeb30e3c55f4a739a339edbfa936a18a9
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2025-08-04 13:32:21 -0400

    add text title functionality and improve web user interface

--- pastebin/src/sql_queries.py
+++ pastebin/src/sql_queries.py
@@ -36,6 +36,7 @@ CREATE INDEX IF NOT EXISTS user_connect_ts_idx ON user_connections (ts)
 CREATE_TABLE_TEXTS = """
 CREATE TABLE IF NOT EXISTS texts (
   text_id VARCHAR(255) PRIMARY KEY,
+  text_title VARCHAR(255),
   text_path VARCHAR(255),
   user_id VARCHAR(40) REFERENCES users(user_id),
   user_ip VARCHAR(15),
@@ -66,8 +67,16 @@ VALUES (%s, %s, %s, %s, %s)
 CREATE_ANONYMOUS_USER = "INSERT INTO users (user_id) VALUES (%s);"
 
 INSERT_TEXT = """
-INSERT INTO texts (text_id, text_path, user_id, user_ip, creation, expiration)
-VALUES (%s, %s, %s, %s, %s, %s)
+INSERT INTO texts (
+    text_id
+    , text_title
+    , text_path
+    , user_id
+    , user_ip
+    , creation
+    , expiration
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
 ;"""
 
 MARK_TEXT_FOR_DELETION = """
@@ -77,7 +86,7 @@ UPDATE texts SET to_be_deleted = true WHERE text_id = %s
 MARK_TEXT_DELETED = "UPDATE texts SET deletion = %s WHERE text_id = %s;"
 
 GET_TEXTS_BY_OWNER = """
-SELECT text_id, creation, expiration FROM texts
+SELECT text_id, text_title, creation, expiration FROM texts
 WHERE user_id = %s AND (deletion IS NULL AND to_be_deleted IS NOT TRUE)
 ORDER BY creation
 ;"""
